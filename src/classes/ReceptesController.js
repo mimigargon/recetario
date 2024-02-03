2512795ac02c77817b8f8b572b38a2db39fb6443
Mimi <mireiag77@gmail.com>
2024-02-03 14:30:59 +0100

    First commit: All done

---
+++ src/classes/ReceptesController.js
@@ -0,0 +1,123 @@
+const APIURL = 'https://app.nocodb.com/api/v2/tables/mdsyufnisuutwst/records';
+const TOKEN = 'd_v_eKtlIQkXvQYKoo5aNObWQQR3Ou3scha5oBMp';
+
+class ReceptesController {
+    constructor() {
+        this.apiUrl = APIURL;
+        this.token = TOKEN;
+    }
+
+    async getAllReceptes() {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async getReceptaById(id) {
+        const response = await fetch(`${this.apiUrl}/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async createRecepta(nom, foto, descripcio, usuari) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                nom,
+                foto,
+                descripcio,
+                usuari
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async updateRecepta(id, nom, foto, descripcio) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                id,
+                nom,
+                foto,
+                descripcio
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async deleteRecepta(id) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                Id:id
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+}
+
+export default ReceptesController;
+
+/*
+// Exemple d'ús:
+const apiUrl = 'https://example.com/api';
+const token = 'el_teu_token_aqui';
+
+const receptesController = new ReceptesController(apiUrl, token);
+
+// Obté totes les receptes
+receptesController.getAllReceptes().then(data => console.log(data));
+
+// Obté una recepta per ID
+const receptaId = 1;
+receptesController.getRecepteById(receptaId).then(data => console.log(data));
+
+// Crea una nova recepta
+const novaRecepta = {
+    nom: 'Paella',
+    foto: 'paella.jpg',
+    descripcio: 'Una deliciosa paella'
+};
+receptesController.createRecepte(novaRecepta.nom, novaRecepta.foto, novaRecepta.descripcio).then(data => console.log(data));
+
+// Actualitza una recepta existent
+const receptaActualitzada = {
+    id: 1,
+    nom: 'Paella Valenciana',
+    foto: 'paella_valenciana.jpg',
+    descripcio: 'La veritable paella valenciana'
+};
+receptesController.updateRecepte(receptaActualitzada.id, receptaActualitzada.nom, receptaActual
+*/
---
+++ src/classes/UsuarisController.js
@@ -0,0 +1,139 @@
+const APIURL = 'https://app.nocodb.com/api/v2/tables/mcpts9331zvsozj/records';
+const TOKEN = 'd_v_eKtlIQkXvQYKoo5aNObWQQR3Ou3scha5oBMp';
+
+class UsuarisController {
+    constructor() {
+        this.apiUrl = APIURL;
+        this.token = TOKEN;
+    }
+
+    async validaUsuari(nom, password){
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        const response_data =  await response.json();
+       
+        const users = response_data.list;
+        const validUser = users.filter(e => e.nom===nom && e.password===password)
+
+        return !!validUser.length;
+    }
+
+
+
+    async getAllUsuaris() {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        return  response.json();
+    }
+
+    async getUsuariById(id) {
+        const response = await fetch(`${this.apiUrl}/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async createUsuari(nom, password) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                nom: nom,
+                password: password,
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async updateUsuari(id, nom, foto, descripcio) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                id,
+                nom,
+                foto,
+                descripcio
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async deleteUsuari(id) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                Id:id
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+}
+
+export default UsuarisController;
+
+/*
+// Exemple d'ús:
+const apiUrl = 'https://example.com/api';
+const token = 'el_teu_token_aqui';
+
+const usuarisController = new ReceptesController(apiUrl, token);
+
+// Obté totes les receptes
+receptesController.getAllReceptes().then(data => console.log(data));
+
+// Obté una recepta per ID
+const receptaId = 1;
+receptesController.getRecepteById(receptaId).then(data => console.log(data));
+
+// Crea una nova recepta
+const novaUsuari = {
+    nom: 'Paella',
+    foto: 'paella.jpg',
+    descripcio: 'Una deliciosa paella'
+};
+receptesController.createRecepte(novaUsuari.nom, novaUsuari.foto, novaUsuari.descripcio).then(data => console.log(data));
+
+// Actualitza una recepta existent
+const receptaActualitzada = {
+    id: 1,
+    nom: 'Paella Valenciana',
+    foto: 'paella_valenciana.jpg',
+    descripcio: 'La veritable paella valenciana'
+};
+receptesController.updateRecepte(receptaActualitzada.id, receptaActualitzada.nom, receptaActual
+*/
